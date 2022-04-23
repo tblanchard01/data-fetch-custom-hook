48b8ceca2077c9cc27e219a8bd952b267c43fde1
Tim <tim.blanchard@hotmail.co.uk>
2022-04-23 17:15:27 +0100

    basic hook working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+const url = "https://dummyjson.com/todos";
 function App() {
+  const [todos, setTodos] = useState([]);
+  console.log(todos);
+  useEffect(() => {
+    fetch(url)
+      .then((data) => data.json())
+      .then(({ todos }) => setTodos(todos));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map(({todo}) =>  <div>{todo}</div>
+      )}
     </div>
   );
 }
