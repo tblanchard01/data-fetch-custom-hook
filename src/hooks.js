534151a9dd1a7864b784b1265ba74e1d310620de
Tim <tim.blanchard@hotmail.co.uk>
2022-04-23 21:39:38 +0100

    refactor settodos

--- src/hooks.js
+++ src/hooks.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 
 const url = "https://dummyjson.com/todos";
+
+async function fetchTodos(setTodos) {
+  let data = await fetch(url);
+  data = await data.json();
+  setTodos(data.todos);
+}
 export function useFetchTodos() {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
-    async function fetchTodos() {
-      let data = await fetch(url);
-      data = await data.json();
-      setTodos(data.todos);
-    }
-    fetchTodos();
+    fetchTodos(setTodos);
   }, []);
   return todos;
 }
